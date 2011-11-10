79c29417a7b77cee77494f53eb79747b38aaa66f
max-m <maximilian.mader@gmx.de>
2011-11-10 23:03:45 +0100

    Auto-Sourceformatting ... :D

--- src/janpath/pong/Hauptfenster.java
+++ src/janpath/pong/Hauptfenster.java
@@ -1,6 +1,5 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in the editor.
  */
 package janpath.pong;
 
@@ -14,59 +13,59 @@ import java.awt.*;
 
 public class Hauptfenster extends JFrame implements ActionListener {
 
-    Spielfeld spielfeld;
-    JMenuBar menu = new JMenuBar();
-    JMenu menuSpiel = new JMenu();
-    JMenuItem menuSpielNeu = new JMenuItem();
-    JMenuItem menuSpielBeenden = new JMenuItem();
+	Spielfeld spielfeld;
+	JMenuBar menu = new JMenuBar();
+	JMenu menuSpiel = new JMenu();
+	JMenuItem menuSpielNeu = new JMenuItem();
+	JMenuItem menuSpielBeenden = new JMenuItem();
 
-    public Hauptfenster() {
-        super("Pong");
+	public Hauptfenster() {
+		super("Pong");
 
-        setSize(816, 661);
+		setSize(816, 661);
 
-        setLocation(
-                (Toolkit.getDefaultToolkit().getScreenSize().width / 2) - 416,
-                (Toolkit.getDefaultToolkit().getScreenSize().height / 2) - 361);
+		setLocation(
+				(Toolkit.getDefaultToolkit().getScreenSize().width / 2) - 416,
+				(Toolkit.getDefaultToolkit().getScreenSize().height / 2) - 361);
 
-        menuSpiel.setText("Spiel");
+		menuSpiel.setText("Spiel");
 
-        menuSpielNeu.setText("Neues Spiel");
-        menuSpielNeu.setAccelerator(KeyStroke.getKeyStroke('N', KeyEvent.CTRL_MASK));
-        menuSpielNeu.addActionListener(this);
+		menuSpielNeu.setText("Neues Spiel");
+		menuSpielNeu.setAccelerator(KeyStroke.getKeyStroke('N', KeyEvent.CTRL_MASK));
+		menuSpielNeu.addActionListener(this);
 
-        menuSpielBeenden.setText("Beenden");
-        menuSpielBeenden.addActionListener(this);
+		menuSpielBeenden.setText("Beenden");
+		menuSpielBeenden.addActionListener(this);
 
-        menuSpiel.add(menuSpielNeu);
-        menuSpiel.add(menuSpielBeenden);
+		menuSpiel.add(menuSpielNeu);
+		menuSpiel.add(menuSpielBeenden);
 
-        menu.add(menuSpiel);
+		menu.add(menuSpiel);
 
-        setJMenuBar(menu);
+		setJMenuBar(menu);
 
-        getContentPane().setLayout(null);
+		getContentPane().setLayout(null);
 
-        spielfeld = new Spielfeld(new Rectangle(0, 0, getWidth() - 6,
-                getHeight() - 48));
+		spielfeld = new Spielfeld(new Rectangle(0, 0, getWidth() - 6,
+				getHeight() - 48));
 
-        getContentPane().add(spielfeld);
+		getContentPane().add(spielfeld);
 
-        setResizable(false);
+		setResizable(false);
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == menuSpielNeu) {
-            spielfeld.schlaeger1.score = 0;
-            spielfeld.schlaeger2.score = 0;
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == menuSpielNeu) {
+			spielfeld.schlaeger1.score = 0;
+			spielfeld.schlaeger2.score = 0;
 
-            spielfeld.resetBall();
+			spielfeld.resetBall();
 
-        } else if (e.getSource() == menuSpielBeenden) {
-            System.exit(0);
-        }
-    }
+		} else if (e.getSource() == menuSpielBeenden) {
+			System.exit(0);
+		}
+	}
 }
