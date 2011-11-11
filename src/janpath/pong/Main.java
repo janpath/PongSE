837844759bcfa4acbb01d89ff3384c52a29f4143
Jan Path <j.path95@googlemail.com>
2011-11-11 18:17:46 +0100

    Added Properties.

--- src/janpath/pong/Main.java
+++ src/janpath/pong/Main.java
@@ -7,19 +7,30 @@ package janpath.pong;
  *
  * @author Jan Path & Maximilian Mader
  */
+import java.io.*;
+
 public class Main {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) throws InterruptedException {
-		//Soundklasse initialisieren
-		PongSound.PONG_POINT.hashCode();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+	//Soundklasse initialisieren
+	PongSound.PONG_POINT.hashCode();
 
-		Hauptfenster fenster = new Hauptfenster();
-		fenster.setVisible(true);
-		Thread.sleep(500);
-		fenster.spielfeld.ball.start();
-
+	//Einstellungen laden
+	File probFile = new File(".properties");
+	if (probFile.isFile()) {
+	    try {
+		PongProperties.prop.load(new FileInputStream(probFile));
+	    } catch (IOException ex) {
+	    }
 	}
+	
+	Hauptfenster fenster = new Hauptfenster();
+	fenster.setVisible(true);
+	Thread.sleep(500);
+	fenster.spielfeld.ball.start();
+
+    }
 }
---
+++ src/janpath/pong/PongProperties.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package janpath.pong;
+
+/**
+ *
+ * @author Jan
+ */
+
+import java.util.Properties;
+
+public interface PongProperties {
+    Properties prop = new Properties();
+}
