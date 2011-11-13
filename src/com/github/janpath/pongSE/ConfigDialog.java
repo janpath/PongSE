9e31f7f8757d09fe717cc42c3a201ef1d188b6ce
Jan Path <j.path95@googlemail.com>
2011-11-13 15:19:45 +0100

    Config-Dialog

---
+++ src/com/github/janpath/pongSE/ConfigDialog.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.janpath.pongSE;
+
+/**
+ *
+ * @author Jan
+ */
+
+import java.awt.event.*;
+import javax.swing.JDialog;
+
+public class ConfigDialog extends JDialog {
+	
+}
--- src/com/github/janpath/pongSE/Hauptfenster.java
+++ src/com/github/janpath/pongSE/Hauptfenster.java
@@ -15,9 +15,11 @@ public class Hauptfenster extends JFrame implements ActionListener {
 
 	Spielfeld spielfeld;
 	JMenuBar menu = new JMenuBar();
-	JMenu menuSpiel = new JMenu();
-	JMenuItem menuSpielNeu = new JMenuItem();
-	JMenuItem menuSpielBeenden = new JMenuItem();
+	JMenu menuSpiel = new JMenu("Spiel");
+	JMenu menuExtras = new JMenu("Extras");
+	JMenuItem menuSpielNeu = new JMenuItem("Neu", 'N');
+	JMenuItem menuSpielBeenden = new JMenuItem("Beenden", 'B');
+	JMenuItem menuExtrasOptionen = new JMenuItem("Optionen", 'O');
 
 	public Hauptfenster() {
 		super("Pong");
@@ -27,20 +29,26 @@ public class Hauptfenster extends JFrame implements ActionListener {
 		setLocation(
 				(Toolkit.getDefaultToolkit().getScreenSize().width / 2) - 416,
 				(Toolkit.getDefaultToolkit().getScreenSize().height / 2) - 361);
+		
+		menuSpiel.setMnemonic('S');
+		menuExtras.setMnemonic('E');
 
-		menuSpiel.setText("Spiel");
-
-		menuSpielNeu.setText("Neues Spiel");
 		menuSpielNeu.setAccelerator(KeyStroke.getKeyStroke('N', KeyEvent.CTRL_MASK));
 		menuSpielNeu.addActionListener(this);
 
-		menuSpielBeenden.setText("Beenden");
+		menuSpielBeenden.setAccelerator(KeyStroke.getKeyStroke('Q', KeyEvent.CTRL_MASK));
 		menuSpielBeenden.addActionListener(this);
+		
+		menuExtrasOptionen.setAccelerator(KeyStroke.getKeyStroke('O', KeyEvent.CTRL_MASK));
+		menuExtrasOptionen.addActionListener(this);
 
 		menuSpiel.add(menuSpielNeu);
 		menuSpiel.add(menuSpielBeenden);
+		
+		menuExtras.add(menuExtrasOptionen);
 
 		menu.add(menuSpiel);
+		menu.add(menuExtras);
 
 		setJMenuBar(menu);
 
@@ -69,6 +77,10 @@ public class Hauptfenster extends JFrame implements ActionListener {
 
 		} else if (e.getSource() == menuSpielBeenden) {
 			System.exit(0);
+		}
+		
+		if (e.getSource() == menuExtrasOptionen) {
+			
 		}
 	}
 }
